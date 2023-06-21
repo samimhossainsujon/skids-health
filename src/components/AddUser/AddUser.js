724a08f0e7dbf70fb45bdbee2f26a76469f18609
samim hossain sujon <samimhossainsanto@gmail.com>
2023-06-22 00:53:00 +0600

    use from hoak added

--- src/components/AddUser/AddUser.js
+++ src/components/AddUser/AddUser.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const AddUser = () => {
+
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
+
+    const onSubmit = data => {
+        console.log(data);
+    }
     return (
         <div>
             add user
--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -11,29 +11,20 @@ const NavigationBar = () => {
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
             </label>
             <ul tabIndex={0} className="menu menu-sm dropdown-content p-2 shadow bg-slate-400 rounded-box w-52">
-              <li><Link>Item 1</Link></li>
-              <li><Link>Item 3</Link></li>
+              <li><Link>Home</Link></li>
+              <li><Link>Add User</Link></li>
             </ul>
           </div>
         </div>
 
-        <div className='flex justify-between items-center'>
-          <div className="navbar-start hidden lg:flex">
-            <ul className="menu menu-horizontal px-1">
-              <li><Link>Item </Link></li>
-              <li><Link>Item 3</Link></li>
-            </ul>
-          </div>
-          <div>
-            <div className="form-control">
-              <input type="text"
-                placeholder="Type here"
-                className="input input-bordered input-secondary w-full max-w-xs" />
-            </div>
-          </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/addUser'>Add User</Link></li>
+          </ul>
         </div>
-      </div >
-    </div >
+      </div>
+    </div>
   );
 };
 
